169c4c9e7b4cf98a5d3a42644bf555427ed5bfb8
thanhlieng <thanh.271020@gmail.com>
2022-03-11 23:02:11 +0700

    add matched avt and message list

--- Screens/Message.js
+++ Screens/Message.js
@@ -6,30 +6,65 @@ import {
   StatusBar,
   FlatList,
   TextInput,
-  Text,
   TouchableOpacity,
   Image,
+  Text,
+  ScrollView,
 } from "react-native";
 import Auth from "../ggAuth/Auth";
 import tw from "tailwind-react-native-classnames";
 import Ionicons from "react-native-vector-icons/Ionicons";
+import { DataTable } from "react-native-paper";
 
-const Item = ({ title }) => (
+const AvtItem = ({ title }) => (
   <View style={styles.listItem}>
-    <Text style={styles.listItemText}>{title}</Text>
+    <Image
+      resizeMode="stretch"
+      style={[tw`h-24 w-16 self-center`, styles.matchAvt]}
+      source={require("../rose.jpg")}
+    />
+    <Text>{title}</Text>
+  </View>
+);
+
+const MessItem = ({ title, message }) => (
+  <View style={styles.messItem}>
+    <Text>{title}</Text>
+    <Text>{message}</Text>
   </View>
 );
 
 const Message = () => {
   const { user, Logout } = Auth();
-  const [Text, setText] = useState("");
-  const [data, setData] = useState(dataOriginal);
-  const dataOriginal = [
-    { id: "1", title: "First item" },
-    { id: "2", title: "Second item" },
-    { id: "3", title: "Third item" },
-    { id: "4", title: "Fourth item" },
-  ];
+  const [text, setText] = useState("");
+  const [data, setData] = useState([
+    { id: "1", title: "First" },
+    { id: "2", title: "Second" },
+    { id: "3", title: "Third" },
+    { id: "4", title: "Fourth" },
+  ]);
+
+  const [dataOriginal, setOri] = useState([
+    { id: "1", title: "First" },
+    { id: "2", title: "Second" },
+    { id: "3", title: "Third" },
+    { id: "4", title: "Fourth" },
+  ]);
+
+  const [dataa, setDataa] = useState([
+    { id: "1", title: "First", message: "oke em" },
+    { id: "2", title: "Second", message: "oke em" },
+    { id: "3", title: "Third", message: "oke em" },
+    { id: "4", title: "Fourth", message: "oke em" },
+  ]);
+
+  const [dataOriginall, setOril] = useState([
+    { id: "1", title: "First", message: "oke em" },
+    { id: "2", title: "Second", message: "oke em" },
+    { id: "3", title: "Third", message: "oke em" },
+    { id: "4", title: "Fourth", message: "oke em" },
+  ]);
+
   const handleSearch = (text) => {
     if (text) {
       const newData = dataOriginal.filter(function (item) {
@@ -40,13 +75,15 @@ const Message = () => {
         return itemData.indexOf(textData) > -1;
       });
       setData(newData);
+      setDataa(newData);
       setText(text);
     } else {
       setData(dataOriginal);
+      setDataa(dataOriginall);
       setText(text);
     }
   };
-  const renderItem = ({ item }) => <Item title={item.title} />;
+  const renderItem = ({ item }) => <AvtItem title={item.title} />;
   return (
     <SafeAreaView style={styles.container}>
       <View style={tw`h-14 flex-row justify-center relative bg-white`}>
@@ -81,15 +118,24 @@ const Message = () => {
           clearButtonMode="always"
         />
       </View>
-      <View style={styles.listContain}>
-        <FlatList
-          data={data}
-          keyExtractor={(item) => item.id}
-          renderItem={renderItem}
-          horizontal={true}
-          style={styles.list}
-        />
-      </View>
+      <ScrollView style={styles.ScrollViewContainer}>
+        <Text style={tw`font-bold`}>Tương hợp</Text>
+        <View style={styles.listContain}>
+          <FlatList
+            data={data}
+            keyExtractor={(item) => item.id}
+            renderItem={renderItem}
+            horizontal={true}
+            style={styles.list}
+          />
+        </View>
+        <Text style={tw`font-bold pt-1`}>Tin nhắn</Text>
+        {dataa.map((item, index) => (
+          <View key={index}>
+            <MessItem title={item.title} message={item.message} />
+          </View>
+        ))}
+      </ScrollView>
     </SafeAreaView>
   );
 };
@@ -106,16 +152,27 @@ const styles = StyleSheet.create({
     borderBottomWidth: 0.6,
   },
   listItem: {
-    marginTop: 10,
     alignItems: "center",
-    paddingHorizontal: 2,
-    backgroundColor: "#fff",
+    flexDirection: "column",
+    paddingEnd: 10,
   },
   listItemText: {
     fontSize: 18,
   },
-  list: {
-    height: 30,
+  listContain: {
+    marginTop: 10,
+  },
+  ScrollViewContainer: {
+    marginHorizontal: 15,
+    paddingTop: 15,
+  },
+  matchAvt: {
+    borderRadius: 40,
+  },
+  messItem: {
+    alignItems: "center",
+    flexDirection: "row",
+    paddingEnd: 10,
   },
 });
 
